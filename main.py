a3dd3ac372ae70752dd8f5274d0a9f63a3cceb43
dhorvathdh <166813817+dhorvathdh@users.noreply.github.com>
2025-10-04 20:03:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ from tools.tools import register_tools
 mcp = FastMCP("myanimelist")
 register_tools(mcp)
 
+def main():
+  mcp.run(transport="stdio")
+
 if __name__ == "__main__":
-    mcp.run(transport="stdio")
+  main()
